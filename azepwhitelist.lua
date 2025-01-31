f51e37e0a5bce7807560e1e15a5f7893c5e7fe09
thisisomeone <rayjuni57@gmail.com>
2025-01-31 17:42:09 +0100

    Update azepwhitelist.lua

--- azepwhitelist.lua
+++ azepwhitelist.lua
@@ -1,18 +1,19 @@
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
-print("hello besties")
+return { 
+	Doors = { 
+		Licensing = true, 
+		Version = "1.2", 
+		Whitelist = { 
+			4729878040, -- uglyburger231 | Owner (<33)
+			716369883, --michalek606 | Owner (<33) 
+			671814299, -- zeverdanoleg325 | Giveaway 
+			3968750952, -- Albert_Keladen | Giveaway 
+			1407008003, -- iuli_flori | Private Reasons ( gave me somehting for it )
+			
+		}, 
+	}, 
+	Blacklist = { 
+		
+		
+	}, 
+} 
+
